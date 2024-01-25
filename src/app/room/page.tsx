63ee2135fdf0d902c85324465966c8d664a98b4a
unknown <dardzinski.d@gmail.com>
2024-01-25 23:02:41 +0100

    rooms card [fix]: added id

---
+++ src/app/room/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationTypes } from '../../provider/store/types';
+import { useSelector } from 'react-redux';
+
+const page = () => {
+  // const { darkMode } = useSelector(
+  //   (store: NavigationTypes) => store.navigation
+  // );
+  return <div>page</div>;
+};
+
+export default page;
